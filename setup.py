bfc36f966b35f90fa83934f6a44c0092e2ecf75b
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-05-13 03:52:58 +0000

    Fixed: Expected end or semicolon (after name and no valid version specifier

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license="MIT",
     install_requires=[
         "Django>=4.0",
-        "spiffworkflow>=^3.0.0"
+        "spiffworkflow>=3.0.0"
     ],
     python_requires='>=3.9',
     classifiers=[
