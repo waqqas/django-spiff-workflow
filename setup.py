f76e1303b399f2474929eeace43162292d6fb4f7
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-05-13 03:58:09 +0000

    Fixed: workflow_tags missing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-spiff-workflow",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     description="Django integration with spiff workflow library",
     author="Waqqas Jabbar",
